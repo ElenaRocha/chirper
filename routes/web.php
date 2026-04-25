caf44ef9c84ff03b9fbc4313ece92e9c7d2b2192
Elena Rocha <ele_r.v@hotmail.com>
2026-04-25 21:19:16 +0200

    Use controller with base route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
 
-Route::get('/', function () {
-    // return view('welcome');
-        return view('home');
-});
+Route::get('/', [ChirpController::class, 'index']);
